dc7ea20c53d1e8539aa30f92ba6d1674e3e011b6
ManoharSaiKondaka <111751471+ManoharSaiKondaka@users.noreply.github.com>
2022-12-15 20:18:31 +0530

    Update assignment_first_test.exs

--- test/assignment_first_test.exs
+++ test/assignment_first_test.exs
@@ -5,14 +5,27 @@ defmodule AssignmentFirstTest do
   end
   #doctest AssignmentFirst
 
-  test "greets the world" do
-    assert AssignmentFirst.hello() == :world
+    test "adding..." do
+    {:ok , output_struct} = AssignmentFirst.add("added task")
+    assert  output_struct.task == "added task"
   end
-  test "check adding" do
-    assert 1+4==5
+  test "updating..." do
+    {:ok , output_struct} = AssignmentFirst.update_task(104,"updated")
+    assert  output_struct.task == "updated"
+    assert  output_struct.id == 104
   end
-  test "adding..." do
-    {:ok , todo_struct} = AssignmentFirst.add("hello world")
-    assert  todo_struct.task == "hello world"
+  test "deleting..." do
+    {:ok , output_struct} = AssignmentFirst.delete_task(99)
+    assert  output_struct.id == 99
+  end
+  test "change status..." do
+    {:ok , output_struct} = AssignmentFirst.toggle_status(99,"complete")
+    assert  output_struct.id == 99
+    assert  output_struct.status == "incomplete"
+  end
+  test "change status2" do
+    {:ok , output_struct} = AssignmentFirst.toggle_status(99,"incomplete")
+    assert  output_struct.id == 99
+    assert  output_struct.status == "complete"
   end
 end
